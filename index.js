9083e2e9c51bc16d7066b66308dcab9bdc942c19
Jennifer Browning <browningjen@yahoo.com>
2018-10-24 11:02:04 -0400

    second attempt

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require("express")
 const port = 3000
 const app = express()
+const updates = [];
 
 app.use(express.static('public'))
 app.use(express.json())
 
 // Fill in your request handlers here
 
+app.post('/updates', function(request, response) {
+    console.log(request.body)
+    if(req.body.clientUpdatess.length > 0) {
+        req.body.clientUpdates.forEach(update => updates.push(update))
+    }
+    newUpdates = updates.slice(req.body.lengthOfLastServerUpdate)
+    res.send({
+        "serverUpdates": newUpdates
+    })
+})
 app.listen(port)
+
+Bitmap.prototype.setColor = function(row, col, color) {
+    this.grid[row][col] = color;
+    this.cells[row][col].style.background = color;
+    let clientUpdate = [row,col,color];
+    clientUpdates.push(clientUpdate)
+}
+Bitmap.prototype.handleEvents = function(row, col, color) {
+    if(event.type === "click") {
+        let row = parseInt(event.currentTarget.dataset.row)
+        (event.currentTarget.dataset.row);
+        let col = parseInt(event.currentTarget.dataset.col)
+        (event.currentTarget.dataset.col)
+    }
+}
+Bitmap.prototype.applyUpdatesFromServer = function(row, col, color) {
+    this.grid[row][col] = color;
+    this.cells[row][col] = style.background = color;
+}
--- public/polling.js
+++ public/polling.js
@@ -1,1 +1,31 @@
 // Add logic to this script to poll server every second for updated pixels.
+// Add logic to this script to poll server every second for updated pixels.
+const timeoutLength = 1000;
+let lengthOfLastServerUpdate = 0;
+const pollServer = async function () {
+    let body = {
+        "clientUpdates": clientUpdates,
+        "lengthOfLastServerUpdate": lengthOfLastServerUpdate    }
+}
+
+body.JSON.stringify(body)
+clientUpdates = [];
+let res = await fetch('/updates', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json; charset=utf-8"
+
+    },
+    body: body
+})
+res = await res.json();
+lengthOfLastServerUpdate =+ res.serverUpdates.length
+
+
+//could also use fetch/then like Aaron's example
+.then(response. => response.json())
+.then(serverUpdates => {
+    console.log(serverUpdates);
+    userIndex = serverUpdates.serverIndex;
+    serverUpdates.updates
+})
